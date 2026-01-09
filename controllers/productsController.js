0248b5aed89df20930a58e68b91ca643d1b78e10
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-09 18:15:54 +0100

    add queries in products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,28 @@
 const connection = require("../data/db");
 
 const index = (req, res) => {
-	res.send("Index");
+	const query = `SELECT * FROM products`;
+	connection.query(query, (err, response) => {
+		if (err) return res.status(500).json({ error: err, message: err.message });
+
+		console.log(response);
+
+		res.json(response);
+	});
 };
 
 const show = (req, res) => {
 	const id = Number(req.params.id);
-	res.send("Show");
+	const query = `SELECT * FROM products WHERE id = ?`;
+
+	connection.query(query, [id], (err, response) => {
+		if (err) return res.status(500).json({ error: err, message: err.message });
+
+		if (response.length === 0)
+			return res.status(404).json({ error: 404, message: "Product Not Found" });
+
+		res.json(response[0]);
+	});
 };
 
 module.exports = { index, show };
