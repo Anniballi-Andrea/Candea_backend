e82437a3ae1de5d5e8734c0b2db7697c169e5642
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-09 18:32:57 +0100

    Add show for orders controller

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -2,11 +2,28 @@ const connection = require("../data/db");
 
 const show = (req, res) => {
 	const id = Number(req.params.id);
-	res.send("Show");
+	const query = `SELECT * FROM orders WHERE id = ?`;
+
+	connection.query(query, [id], (err, response) => {
+		if (err) return res.status(500).json({ error: err, message: err.message });
+
+		if (response.length === 0)
+			return res.status(404).json({ error: 404, message: "Order Not Found" });
+
+		res.json(response[0]);
+	});
 };
 
 const store = (req, res) => {
-	res.send("Store");
+	const { data } = req.body;
+
+	if (!data) {
+		return res
+			.status(400)
+			.json({ error: true, message: "Something is wrong with the input" });
+	}
+
+	res.send(`Stored: ${data}`);
 };
 
 module.exports = { show, store };
